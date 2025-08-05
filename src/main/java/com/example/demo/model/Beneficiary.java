21fc7a21a3172e113a2298e36879010856032d0b
Ando Mamy <hei.ando.14@gmail.com>
2025-08-05 06:58:10 -0400

    (model)feat: create Donor and Beneficary

---
+++ src/main/java/com/example/demo/model/Beneficiary.java
@@ -0,0 +1,4 @@
+package com.example.demo.model;
+
+public record Beneficiary(String email, String fullName) {
+}
---
+++ src/main/java/com/example/demo/model/Donor.java
@@ -0,0 +1,4 @@
+package com.example.demo.model;
+
+public record Donor(String email, String fullName) {
+}
